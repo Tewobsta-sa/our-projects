06c93f22b3776457c53106ab03e78d3151d8a7dc
NUNIYA <157139125+NUNIYA@users.noreply.github.com>
2024-03-23 08:58:19 -0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello nunu welcome!");
-System.out.println("well thnx for having me so how does this thing work how did you upload the file")
+System.out.println("well thnx for having me so how does this thing work how did you upload the file");
         }
     }
 }
